6cacc07a7344b61cc75d6fe38ed6a6fd3c681fee
Russel Yasol <russel.yasol@wvsu.edu.ph>
2026-03-21 18:28:02 +0800

    feat(system-settings): add Software Update card to settings page

--- components/system-settings/system-settings.tsx
+++ components/system-settings/system-settings.tsx
@@ -3,6 +3,7 @@
 import { useSystemSettings } from "@/hooks/use-system-settings";
 import SystemSettingsCard from "@/components/system-settings/system-settings-card";
 import ScheduledOperationsCard from "@/components/system-settings/scheduled-operations-card";
+import SoftwareUpdateCard from "@/components/system-settings/software-update-card";
 
 const SystemSettings = () => {
   const hookData = useSystemSettings();
@@ -15,6 +16,9 @@ const SystemSettings = () => {
       <div className="grid grid-cols-1 @3xl/main:grid-cols-2 grid-flow-row gap-4">
         <SystemSettingsCard {...hookData} />
         <ScheduledOperationsCard {...hookData} />
+        <div className="@3xl/main:col-span-2">
+          <SoftwareUpdateCard />
+        </div>
       </div>
     </div>
   );
